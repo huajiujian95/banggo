7bdf34e1d7de66516051c02ae2fc3a309e75a104
huajiujian <740145822@qq.com>
2020-01-11 15:44:06 +0800

    111

--- banggo/client/src/lib/form_denglu.js
+++ banggo/client/src/lib/form_denglu.js
@@ -1,7 +1,51 @@
-// $(() => {
-//     $("t2").click(function () { 
-//         $(this).addClass("cstyle").siblings().removesClass("cstyle");
+$(() => {
+    $(".t1").click(function() {
+        $(this).addClass("cstyle").siblings().removeClass("cstyle");
+        $("#login_type_name").text("用户名：");
+        $("#login_type_pwd").text("登录密码：");
+    })
+    $(".t2").click(function() { 
+        $(this).addClass("cstyle").siblings().removeClass("cstyle");
+        $("#login_type_name").text("手机号：");
+        $("#login_type_pwd").text("登录密码：");
+    });
+    $(".t3").click(function() {
+        $(this).addClass("cstyle").siblings().removeClass("cstyle");
+        $("#login_type_name").text("卡 号：");
+        $("#login_type_pwd").text("卡密码：");
+    });
 
-//         $("#login_type_name")
-//     });
-// })
+    $("#log_btn").click(function() {
+        let username = $.trim($("#username").val());
+        let password = $.trim($("#password").val());
+
+        if (username.length == 0) {
+            alert("请输入用户名");
+            return;
+        }
+
+        if (password.length == 0) {
+            alert("请输入密码")
+            return;
+        }
+
+        let data = {
+            username,
+            password: md5(password).substr(0,10)
+        }
+
+        $.ajax({
+            type: "post",
+            url: "../../../server/form_denglu.php",
+            data,
+            dataType: "json",
+            success: function (response) {
+                if (response.status == "success") {
+                    window.location,href = "http://www.banggo.com"
+                } else {
+                    alert(response.msg);
+                }
+            }
+        });
+    })
+})
---
+++ banggo/server/form_denglu.php
@@ -0,0 +1,7 @@
+<?php
+$username = $_REQUEST["username"];
+$password = $_REQUEST["password"];
+
+$db = mysql_connet("127.0.0.1","root","","banggo");
+
+?>
